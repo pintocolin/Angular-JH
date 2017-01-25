d545480adb806bee4e2d33ab31ae938c81293f09
Colin <colin.pinto@microtrac.com>
2017-01-25 15:02:19 -0500

    Added logic to process the text box and click event

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,9 +6,27 @@ angular.module("LunchCheck", [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
-    $scope.LunchMessage = "test";
-    $scope.yyy ="" ;
+    $scope.LunchMessage = "";
     $scope.lunchList = "";
+
+    $scope.processLunchList = function (){
+    console.log("=processing lunch List=");
+
+    var lunch = $scope.lunchList;
+    var List =lunch.split(',');
+
+    console.log(lunch);
+    console.log(List);
+
+    console.log(List.length);
+    if(List.length == 1 && List[0] =="")
+        $scope.LunchMessage = "Please enter data first";
+    else if(List.length <4 || (List.length ==4 && List[3] ==""))
+        $scope.LunchMessage = "Enjoy!";
+    else
+        $scope.LunchMessage = "Too much!";
     }
 
+}
+//processLunchList( );
 }  )();
