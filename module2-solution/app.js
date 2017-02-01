0696e18aff15e55aac2c22302355ed65eba1f4a5
Colin <colin.pinto@microtrac.com>
2017-02-01 14:28:34 -0500

    added js code

---
+++ module2-solution/app.js
@@ -0,0 +1,53 @@
+(function(){
+'use strict';
+// app.js
+angular.module("ShoppingListCheckOff",[])
+.controller("ToBuyController", ToBuyController)
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+ToBuyController.$injector =['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
+
+    toBuy.items = ShoppingListCheckOffService.getBuyItems();
+    toBuy.buy = function(index){
+        ShoppingListCheckOffService.buyItem(index);
+    }
+}
+
+AlreadyBoughtController.$injector = ["ShoppingListCheckOffService"];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+    var bought = this;
+    bought.items = ShoppingListCheckOffService.boughtItems();
+}
+
+ShoppingListCheckOffService.$injector = ["scope"]
+function ShoppingListCheckOffService(){
+    var checkService = this;
+
+
+    // List of ToBuy items
+    var toBuyItems = [ 
+        {name:'cases of beer', quantity:3}, 
+        {name:'bucket of chicken wings', quantity:3},
+        {name:'bags of pretrels', quantity:4},
+        {name:'bags of chips', quantity:5},
+        {name:'corn chips', quantity:6},
+        {name:'cases of coke', quantity:4},
+        ];
+        // List of Bought itms
+    var boughtItems = [];
+checkService.buyItem = function(itemIndex){
+    var item = toBuyItems.splice(itemIndex,1)[0];
+    boughtItems.push(item);
+}
+checkService.getBuyItems = function(){
+    return toBuyItems;
+}
+checkService.boughtItems = function(){
+    return boughtItems;
+}
+}
+
+})()
