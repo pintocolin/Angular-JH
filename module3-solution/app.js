100512332f24b09a3534395d791fc54878855477
Colin Pinto <Colin Pinto>
2017-02-09 17:03:38 -0500

    added initial code

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,53 +1,25 @@
 (function(){
 'use strict';
 // app.js
-angular.module("ShoppingListCheckOff",[])
-.controller("ToBuyController", ToBuyController)
-.controller("AlreadyBoughtController", AlreadyBoughtController)
-.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+angular.module("NarrowItDownApp",[])
+.controller("NarrowItDownController", NarrowItDownController)
+.service("MenuSearchService", MenuSearchService);
 
-ToBuyController.$injector =['ShoppingListCheckOffService'];
-function ToBuyController(ShoppingListCheckOffService){
-    var toBuy = this;
-
-    toBuy.items = ShoppingListCheckOffService.getBuyItems();
-    toBuy.buy = function(index){
-        ShoppingListCheckOffService.buyItem(index);
-    }
-}
-
-AlreadyBoughtController.$injector = ["ShoppingListCheckOffService"];
-function AlreadyBoughtController(ShoppingListCheckOffService){
-    var bought = this;
-    bought.items = ShoppingListCheckOffService.boughtItems();
-}
-
-ShoppingListCheckOffService.$injector = ["scope"]
-function ShoppingListCheckOffService(){
-    var checkService = this;
+NarrowItDownController.$injector =["MenuSearchService"];
+function NarrowItDownController(MenuSearchService){
+    var menu= this;
 
 
-    // List of ToBuy items
-    var toBuyItems = [ 
-        {name:'cases of beer', quantity:3}, 
-        {name:'bucket of chicken wings', quantity:3},
-        {name:'bags of pretrels', quantity:4},
-        {name:'bags of chips', quantity:5},
-        {name:'bags cheese curls', quantity:6},
-        {name:'cases of coke', quantity:4},
-        ];
-        // List of Bought itms
-    var boughtItems = [];
-checkService.buyItem = function(itemIndex){
-    var item = toBuyItems.splice(itemIndex,1)[0];
-    boughtItems.push(item);
-}
-checkService.getBuyItems = function(){
-    return toBuyItems;
-}
-checkService.boughtItems = function(){
-    return boughtItems;
-}
+}   
+
+
+
+MenuSearchService.$injector = ["scope"];
+function MenuSearchService(){
+    var menuService = this;
+
+
+    
 }
 
 })()
