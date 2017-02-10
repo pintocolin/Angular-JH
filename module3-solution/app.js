a2609a621418b6c1d375eb7f011cc21c32ab7c98
Colin Pinto <Colin Pinto>
2017-02-09 23:32:15 -0500

    added logic http service

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,23 +3,59 @@
 // app.js
 angular.module("NarrowItDownApp",[])
 .controller("NarrowItDownController", NarrowItDownController)
-.service("MenuSearchService", MenuSearchService);
+.service("MenuSearchService", MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItemsDirective);
 
 NarrowItDownController.$injector =["MenuSearchService"];
 function NarrowItDownController(MenuSearchService){
     var menu= this;
 
+    var searchTerm = "chicken";
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+
+    promise.then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
 
 }   
 
+MenuSearchService.$injector = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath){
+    var service = this;
 
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      params: {
+        //category: shortName
+      }
+    });
 
-MenuSearchService.$injector = ["scope"];
-function MenuSearchService(){
-    var menuService = this;
-
-
+    return response;
+  };
     
 }
+function  () {
+  var ddo = {
+    templateUrl: 'foundItemsTemplate.html',
+    restrict: 'E',
+    scope: {
+      items: '<foundItems',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
 
-})()
+  return ddo;
+}
+
+})()  
